103fac4593b47367ece1268a5fefad575fc0db2c
Chris <christianrod97@gmail.com>
2021-01-24 03:28:30 -0500

    Improved files

--- functions/index.js
+++ functions/index.js
@@ -5,16 +5,8 @@ const app = require('express')();
 
 admin.initializeApp();
 
-var firebaseConfig = {
-    apiKey: "AIzaSyBw9LYvSkgKjB-Zh6NBcpwmLq3_N06ZEQ4",
-    authDomain: "gymbuds-34a8c.firebaseapp.com",
-    databaseURL: "https://gymbuds-34a8c-default-rtdb.firebaseio.com",
-    projectId: "gymbuds-34a8c",
-    storageBucket: "gymbuds-34a8c.appspot.com",
-    messagingSenderId: "636927856126",
-    appId: "1:636927856126:web:f42607221279a340fac4af",
-    measurementId: "G-MBN660MS80"
-};
+
+var firebaseConfig = require('./firebaseConfig');
 
 const firebase = require('firebase');
 firebase.initializeApp(firebaseConfig);
