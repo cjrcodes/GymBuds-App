9d46f659b228e5ccc4e5f2d13933d32b4a25313d
Chris <christianrod97@gmail.com>
2021-03-20 17:49:18 -0400

    Fixed change image upload

--- functions/index.js
+++ functions/index.js
@@ -1,155 +1,177 @@
 const functions = require("firebase-functions");
 
-const app = require('express')();
+const app = require("express")();
 
-const { db } = require('./util/admin');
+const { db } = require("./util/admin");
 
-const { 
-     getAllBudcalls,
-     postOneBudcall, 
-     getBudcall, 
-     commentOnBudcall,
-     likeBudcall,
-     unlikeBudcall,
-     deleteBudcall 
-    } = require('./handlers/budcalls');
-const { 
-    signup, 
-    login, 
-    uploadImage, 
-    addUserDetails, 
-    getAuthenticatedUser,
-    getUserDetails,
-    markNotificationsRead 
-} = require('./handlers/users');
+const cors = require("cors");
+app.use(cors());
 
-const FBAuth = require('./util/fbAuth');
+const {
+  getAllBudcalls,
+  postOneBudcall,
+  getBudcall,
+  commentOnBudcall,
+  likeBudcall,
+  unlikeBudcall,
+  deleteBudcall,
+} = require("./handlers/budcalls");
+const {
+  signup,
+  login,
+  uploadImage,
+  addUserDetails,
+  getAuthenticatedUser,
+  getUserDetails,
+  markNotificationsRead,
+} = require("./handlers/users");
+
+const FBAuth = require("./util/fbAuth");
 
 //Budcall routes
-app.get('/budcalls', getAllBudcalls);
-app.post('/budcall', FBAuth, postOneBudcall);
-app.get('/budcall/:budcallId', getBudcall);
-app.delete('/budcall/:budcallId', FBAuth, deleteBudcall);
+app.get("/budcalls", getAllBudcalls);
+app.post("/budcall", FBAuth, postOneBudcall);
+app.get("/budcall/:budcallId", getBudcall);
+app.delete("/budcall/:budcallId", FBAuth, deleteBudcall);
 
-app.get('/budcall/:budcallId/like', FBAuth, likeBudcall);
-app.get('/budcall/:budcallId/unlike', FBAuth, unlikeBudcall);
-app.post('/budcall/:budcallId/comment', FBAuth, commentOnBudcall);
+app.get("/budcall/:budcallId/like", FBAuth, likeBudcall);
+app.get("/budcall/:budcallId/unlike", FBAuth, unlikeBudcall);
+app.post("/budcall/:budcallId/comment", FBAuth, commentOnBudcall);
 
 //users routes
-app.post('/signup', signup);
-app.post('/login', login);
-app.post('/user/image', FBAuth, uploadImage);
-app.post('/user', FBAuth, addUserDetails);
-app.get('/user', FBAuth, getAuthenticatedUser);
-app.get('/user/:handle', getUserDetails);
-app.post('/notifications', FBAuth, markNotificationsRead);
+app.post("/signup", signup);
+app.post("/login", login);
+app.post("/user/image", FBAuth, uploadImage);
+app.post("/user", FBAuth, addUserDetails);
+app.get("/user", FBAuth, getAuthenticatedUser);
+app.get("/user/:handle", getUserDetails);
+app.post("/notifications", FBAuth, markNotificationsRead);
 
-exports.api = functions.https.onRequest(app);
+exports.api = functions.region("us-central1").https.onRequest(app);
 
 exports.createNotificationOnLike = functions
-.region('us-central1')
-.firestore.document('likes/{id}')
-    .onCreate((snapshot) => {
-        db.doc(`/budcalls/${snapshot.data().budcallId}`).get()
-        .then(doc => {
-            if(doc.exists && doc.data().userHandle !== snapshot.data().userHandle){
-                return db.doc(`/notifications/${snapshot.id}`).set({
-                    createdAt: new Date().toISOString(),
-                    recipient: doc.data().userHandle,
-                    sender: snapshot.data().userHandle,
-                    type: 'like',
-                    read: false,
-                    budcallId: doc.id
-                });
-            }
-        })
-     
-        .catch((err) => 
-            console.error(err));
-    });
+  .region("us-central1")
+  .firestore.document("likes/{id}")
+  .onCreate((snapshot) => {
+    db.doc(`/budcalls/${snapshot.data().budcallId}`)
+      .get()
+      .then((doc) => {
+        if (
+          doc.exists &&
+          doc.data().userHandle !== snapshot.data().userHandle
+        ) {
+          return db.doc(`/notifications/${snapshot.id}`).set({
+            createdAt: new Date().toISOString(),
+            recipient: doc.data().userHandle,
+            sender: snapshot.data().userHandle,
+            type: "like",
+            read: false,
+            budcallId: doc.id,
+          });
+        }
+      })
+
+      .catch((err) => console.error(err));
+  });
 
 exports.deleteNotificationOnUnlike = functions
-.region('us-central1')
-.firestore.document('likes/{id}')
-.onDelete((snapshot) => {
-    return db.doc(`/notifications/${snapshot.id}`)
-    .delete()
-    .catch(err => {
+  .region("us-central1")
+  .firestore.document("likes/{id}")
+  .onDelete((snapshot) => {
+    return db
+      .doc(`/notifications/${snapshot.id}`)
+      .delete()
+      .catch((err) => {
         console.error(err);
         return;
-    });
-});
+      });
+  });
 
 exports.createNotificationOnComment = functions
-.region('us-central1')
-.firestore.document('comments/{id}')
-.onCreate((snapshot) => {
-    return db.doc(`/budcalls/${snapshot.data().budcallId}`).get()
-        .then(doc => {
-            if(doc.exists && doc.data().userHandle !== snapshot.data().userHandle){
-                return db.doc(`/notifications/${snapshot.id}`).set({
-                    createdAt: new Date().toISOString(),
-                    recipient: doc.data().userHandle,
-                    sender: snapshot.data().userHandle,
-                    type: 'comment',
-                    read: false,
-                    budcallId: doc.id
-                });
-            }
-        })
-      
-        .catch(err => {
-            console.error(err);
-            return;
-        });
-});
+  .region("us-central1")
+  .firestore.document("comments/{id}")
+  .onCreate((snapshot) => {
+    return db
+      .doc(`/budcalls/${snapshot.data().budcallId}`)
+      .get()
+      .then((doc) => {
+        if (
+          doc.exists &&
+          doc.data().userHandle !== snapshot.data().userHandle
+        ) {
+          return db.doc(`/notifications/${snapshot.id}`).set({
+            createdAt: new Date().toISOString(),
+            recipient: doc.data().userHandle,
+            sender: snapshot.data().userHandle,
+            type: "comment",
+            read: false,
+            budcallId: doc.id,
+          });
+        }
+      })
 
-exports.onUserImageChange = functions.region('us-central1').firestore.document('/users/{userId}').
-onUpdate((change) => {
+      .catch((err) => {
+        console.error(err);
+        return;
+      });
+  });
+
+exports.onUserImageChange = functions
+  .region("us-central1")
+  .firestore.document("/users/{userId}")
+  .onUpdate((change) => {
     console.log(change.before.data());
     console.log(change.after.data());
-   if(change.before.data().imageUrl !== change.after.data().imageUrl){
-       console.log("Image has changed"); 
-       let batch = db.batch();
-       return db.collection('budcalls').where('userHandle', '==', change.before.data().handle).get()
-       .then((data) => {
-           data.forEach(doc => {
-               const budcall = db.doc(`/budcalls/${doc.id}`);
-               batch.update(budcall, { userImage: change.after.data().imageUrl});
-           });
-           return batch.commit();
-       });
-   } else return true;
-});
+    if (change.before.data().imageUrl !== change.after.data().imageUrl) {
+      console.log("Image has changed");
+      let batch = db.batch();
+      return db
+        .collection("budcalls")
+        .where("userHandle", "==", change.before.data().handle)
+        .get()
+        .then((data) => {
+          data.forEach((doc) => {
+            const budcall = db.doc(`/budcalls/${doc.id}`);
+            batch.update(budcall, { userImage: change.after.data().imageUrl });
+          });
+          return batch.commit();
+        });
+    } else return true;
+  });
 
 exports.onBudcallDelete = functions
-.region('us-central1')
-.firestore.document('/budcalls/{budcallId}')
-.onDelete((snapshot, context) => {
+  .region("us-central1")
+  .firestore.document("/budcalls/{budcallId}")
+  .onDelete((snapshot, context) => {
     const budcallId = context.params.budcallId;
     const batch = db.batch();
 
     return db
-    .collection('comments')
-    .where('budcallId', '==', budcallId).get()
-    .then(data => {
-        data.forEach(doc => {
-            batch.delete(db.doc(`/comments/${doc.id}`));
+      .collection("comments")
+      .where("budcallId", "==", budcallId)
+      .get()
+      .then((data) => {
+        data.forEach((doc) => {
+          batch.delete(db.doc(`/comments/${doc.id}`));
         });
-        return db.collection('likes').where('budcallId', '==', budcallId).get();
-    }).then(data => {
-        data.forEach(doc => {
-            batch.delete(db.doc(`/likes/${doc.id}`));
+        return db.collection("likes").where("budcallId", "==", budcallId).get();
+      })
+      .then((data) => {
+        data.forEach((doc) => {
+          batch.delete(db.doc(`/likes/${doc.id}`));
         });
-        return db.collection('notifications').where('budcallId', '==', budcallId).get();
-    })
-    .then(data => {
-        data.forEach(doc => {
-            batch.delete(db.doc(`/notifications/${doc.id}`));
+        return db
+          .collection("notifications")
+          .where("budcallId", "==", budcallId)
+          .get();
+      })
+      .then((data) => {
+        data.forEach((doc) => {
+          batch.delete(db.doc(`/notifications/${doc.id}`));
         });
         return batch.commit();
-    })
-    .catch((err) => {
+      })
+      .catch((err) => {
         console.error(err);
-    });
-});
+      });
+  });
